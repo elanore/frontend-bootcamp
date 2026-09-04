f195bd3238cbb1db9b563ae61d8079502037a1a1
elanore <elanorejoseph@gmail.com>
2026-09-04 21:01:02 +0530

    switch statement

--- projects/javascript/javascript-notes2.md
+++ projects/javascript/javascript-notes2.md
@@ -1,1 +1,24 @@
 # Switch statement
+
+when you want to compare one vlaues to multiple values .
+like week day
+switch (day){
+case 'monday':
+console.log("hello monday");
+break;
+case 'Tuesday':
+console.log("hello tuesday");
+break;
+
+---
+
+}
+compare the day to case strict, you can add a default incase all other cases break.
+if two cases have same actions then example :
+case 'wednesday':
+case 'thursday':
+{
+actions;
+}
+
+with ifelse
---
+++ projects/javascript/switchStatement.js
@@ -0,0 +1,27 @@
+{
+  const day = "monday";
+
+  switch (day) {
+    case "monday":
+      console.log("Today is Monday");
+      break;
+    case "tuesday":
+      console.log("Today is Tuesday");
+      break;
+    case "wednesday":
+    case "thursday":
+      console.log("write code examples for Wednesday and Thursday");
+      break;
+    case "friday":
+      console.log("Today is Friday");
+      break;
+    case "saturday":
+      console.log("Today is Saturday");
+      break;
+    case "sunday":
+      console.log("Today is Sunday");
+      break;
+    default:
+      console.log("Invalid day");
+  }
+}
